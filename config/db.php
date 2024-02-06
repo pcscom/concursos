00d60290b358464664f17adfefa957fb683e0f92
CrespiJ <41802950+CrespiJ@users.noreply.github.com>
2024-02-06 09:54:06 -0300

    Example db.php

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=[demoDbName]',
+    'username' => [demoUser],
+    'password' => [demoPassword],
+    'charset' => 'utf8',
+];
