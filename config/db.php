bcaa9943bb8828908946a8befaeb2879cf5fd9d5
agustinbaez <agustinbaez@multiplica.com>
2024-03-01 23:39:20 -0300

    config db

--- config/db.php
+++ config/db.php
@@ -1,15 +1,9 @@
 <?php
 
 return [
-    // 'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=db:3306;dbname=concursos',
-    // 'username' => 'concursos',
-    // 'password' => '1R27pfeGVOrI',
-    // 'charset' => 'utf8',
-
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1:3306;dbname=concursos',
-    'username' => 'admin',
-    'password' => 'Zaq12wsx',
+    'dsn' => 'mysql:host=db:3306;dbname=concursos',
+    'username' => 'concursos',
+    'password' => '1R27pfeGVOrI',
     'charset' => 'utf8',
 ];
